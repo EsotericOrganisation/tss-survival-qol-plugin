1bc48640afb7d1ec3dd7d4546ad33607abf8e947
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-07-12 03:32:32 +0100

    Fix build config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
-plugins {
-    java
-    application
+import xyz.jpenilla.resourcefactory.bukkit.BukkitPluginYaml
 
-    id("io.papermc.paperweight.userdev") version "1.5.5"
-    id("xyz.jpenilla.run-paper") version "2.1.0"
+plugins {
+  `java-library`
+  id("io.papermc.paperweight.userdev") version "1.7.1"
+  id("xyz.jpenilla.run-paper") version "2.3.0" // Adds runServer and runMojangMappedServer tasks for testing
+  id("xyz.jpenilla.resource-factory-bukkit-convention") version "1.1.1" // Generates plugin.yml based on the Gradle config
 }
 
 group = "net.slqmy"
@@ -11,55 +12,25 @@ version = "0.1"
 description = "A plugin template for use as a dependant plugin of the TSS-Core plugin."
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
-}
-
-repositories {
-    mavenCentral()
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 dependencies {
     compileOnly(files("../TSS-Core/build/libs/tss_core-0.1-dev-all.jar"))
 
-    paperweight.paperDevBundle("1.20.1-R0.1-SNAPSHOT")
+    paperweight.paperDevBundle("1.21-R0.1-SNAPSHOT")
 }
 
 tasks {
-    assemble {
-        dependsOn(reobfJar)
-    }
-
     compileJava {
         options.encoding = Charsets.UTF_8.name()
-        options.release.set(17)
-    }
-
-    javadoc {
-        options.encoding = Charsets.UTF_8.name()
-    }
-
-    processResources {
-        filteringCharset = Charsets.UTF_8.name()
-
-        val props = mapOf(
-                "name" to project.name,
-                "version" to project.version,
-                "description" to project.description,
-                "apiVersion" to "1.20"
-        )
-
-        inputs.properties(props)
-
-        filesMatching("plugin.yml") {
-            expand(props)
-        }
-    }
-
-    runServer {
-        minecraftVersion("1.20.1")
+        options.release.set(21)
     }
 }
 
-application {
-    mainClass.set("TSSPlugin")
+bukkitPluginYaml {
+  main = "net.slqmy.tss_plugin_template.TSSPlugin"
+  load = BukkitPluginYaml.PluginLoadOrder.STARTUP
+  authors.add("Slqmy")
+  apiVersion = "1.21"
 }
